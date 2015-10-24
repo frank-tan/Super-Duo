79d21f485032bf62b91e6f2a165fa8c3dbe2c516
Frank Tan <francis.tine@gmail.com>
2015-10-24 17:03:15 +0800

    Added leaky canary library to track memory leak

---
+++ alexandria/app/src/main/java/it/jaschke/alexandria/AlexandriaApplication.java
@@ -0,0 +1,15 @@
+package it.jaschke.alexandria;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by tan on 24/10/2015.
+ */
+public class AlexandriaApplication extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
