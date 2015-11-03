838010a3cb369ce13e1be08f615cc3c2060e1045
Frank Tan <francis.tine@gmail.com>
2015-11-03 23:00:28 +0800

    Added a simple widget

---
+++ football_scores/app/src/main/java/barqsoft/footballscores/widget/FootballAppWidgetProvider.java
@@ -0,0 +1,44 @@
+package barqsoft.footballscores.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.R;
+import barqsoft.footballscores.ui.MainActivity;
+
+/**
+ * Created by tan on 3/11/2015.
+ */
+public class FootballAppWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.scores_list_item);
+            views.setTextViewText(R.id.home_name,"Frank");
+            views.setTextViewText(R.id.away_name,"Ariel");
+            views.setImageViewResource(R.id.home_crest, R.drawable.ic_launcher);
+            views.setImageViewResource(R.id.away_crest,R.drawable.ic_launcher);
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
---
+++ football_scores/app/src/main/java/barqsoft/footballscores/widget/WidgetService.java
@@ -0,0 +1,7 @@
+package barqsoft.footballscores.widget;
+
+/**
+ * Created by tan on 3/11/2015.
+ */
+public class WidgetService {
+}
